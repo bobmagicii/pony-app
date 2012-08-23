94b856ba815c778ee5e04408e0c6d98f8d75bd8f
Bob Majdak Jr <bob@opsat.net>
2012-08-23 00:20:35 -0500

    massive restructure to be more oop and conducive to multipony support, or at the very least changing ponies on the fly easier.

--- PonyApp/MainWindow.xaml.cs
+++ PonyApp/MainWindow.xaml.cs
@@ -20,39 +20,13 @@ namespace PonyApp {
 
 	public partial class MainWindow:Window {
 
-		// this timer is for moving the window around. unfortunately if the pc is too busy
-		// or i set it too fast, it slow down the gif rendering quite a bit as it shares
-		// the ui thread. problem is that system.timers.timer didn't have permissions to
-		// update the object owned by another thread.
-		private DispatcherTimer WindowTimer;
-
-		// this timer is for when the pony decides it should do something else. time till
-		// bordem basically.
-		private DispatcherTimer ChoiceTimer;
-
-		// the current action the pony is taking and the current direction. these need to
-		// get moved to the pony class when i get to making the pony class work.
-		private int CurrentAction;
-		private int CurrentDirection;
-		private BitmapImage CurrentImage;
-
-		// just some things i stored because i can.
-		private double ScreenWidth;
-		private double ScreenHeight;
+		private Pony Pony;
 
 		public MainWindow() {
 			InitializeComponent();
 
 			// enable moving the window by dragging anywhere.
 			// this.MouseLeftButtonDown += new MouseButtonEventHandler(OnWindowDragAction);
-
-			// prepare some values.
-			this.ScreenWidth = SystemParameters.VirtualScreenWidth;
-			this.ScreenHeight = SystemParameters.VirtualScreenHeight;
-			this.WindowTimer = null;
-			this.ChoiceTimer = null;
-			this.CurrentAction = Pony.STAND;
-			this.CurrentDirection = Pony.RIGHT;
 
 			// set the window position.
 			this.Left = 0;
@@ -63,177 +37,9 @@ namespace PonyApp {
 			this.Title = "Pony";
 
 			// go pony go
-			this.ChooseWhatDo(Pony.TROT,Pony.RIGHT);
-			this.Top = this.ScreenHeight - (34 + this.Height);
-
-			// setup a timer for choosing what to do.
-			this.ChoiceTimer = new DispatcherTimer(DispatcherPriority.Normal,this.Dispatcher);
-			this.ChoiceTimer.Interval = TimeSpan.FromSeconds(1);
-			this.ChoiceTimer.Tick += new EventHandler(this.ChooseWhatDo);
-			this.ChoiceTimer.Start();
-
-		}
-
-
-
-		private void CurrentActionStop() {
-
-			if(this.WindowTimer != null) {
-				this.WindowTimer.Stop();
-				this.WindowTimer = null;
-				// this is me assuming there is a garbage collector in this
-				// language and that it is doing its goddamn job :)
-			}
-
-		}
-
-		/* void MainWindow.ChooseWhatToDo(int action, int direction);
-		 * this version of the method is the authority. what it is told is what
-		 * the pony is made to do */
-
-		private void ChooseWhatDo(int action,int direction) {
-			this.CurrentActionStop();
-
-			this.CurrentAction = action;
-			this.CurrentDirection = direction;
-
-			switch(action) {
-				case Pony.TROT:
-					this.Trot(direction);
-					break;
-
-				case Pony.STAND:
-					this.Stand(direction);
-					break;
-			}
-
-			Trace.WriteLine("choice: " + this.CurrentAction.ToString() + ":" + this.CurrentDirection.ToString());
-		}
-
-		/* void MainWindow.ChooseWhatToDo(Object sender EventArgs event);
-		 * this version of the method is for calling by the ChoiceTimer. It resets
-		 * the choice timer and then allows the pony to decide what to do. */
-
-		private void ChooseWhatDo(Object s,EventArgs e) {
-			int time = 0;
-			Random rng = new Random();
-
-			// stop choice timer.
-			this.ChoiceTimer.Stop();
-
-			// choose what to do.
-			this.ChooseWhatDo();
-
-			// restart the choice timer. ponies like to be lazy so
-			// choose sooner if we are doing something active.
-			if(this.CurrentAction == Pony.TROT) time = rng.Next(3,5);
-			else time = rng.Next(12,20);
-
-			this.ChoiceTimer.Interval = TimeSpan.FromSeconds(time);
-			this.ChoiceTimer.Start();
-		}
-
-		/* void MainWindow.ChooseWhatToDo(void);
-		 * this version of the method is the default to allow the pony to decide
-		 * what it wants to do next. */
-
-		private void ChooseWhatDo() {
-			int[] choice = this.ChoiceByPersonality();
-			this.ChooseWhatDo(choice[0],choice[1]);
-		}
-
-		/* int-array MainWindow.ChoiceByPersonality(void);
-		 * choose what to do based on the pony personality. at the moment i only
-		 * have one personality, the personality i want. it will choose the new
-		 * action and direction. Returns an array 0 being the action she selected
-		 * and 1 being the direction chosen. */
-
-		private int[] ChoiceByPersonality() {
-			Random rng = new Random();
-			bool undecided = false;
-			int action = 0;
-			int direction = rng.Next(1,3);
-
-			do {
-
-				action = rng.Next(1,3);
-				direction = rng.Next(1,3);
-				undecided = false;
-
-				// pony generally like be lazy. if she is doing somethng lazy there is a
-				// greater chance she will not want to do something active.
-				if(Pony.IsActionActive(action) && !Pony.IsActionActive(this.CurrentAction)) {
-					if(rng.Next(1,101) <= 25) undecided = true;
-				}
-
-				// if pony is doing something active and will continue to do so then there
-				// is a higher chance she will not change directions.
-				if(Pony.IsActionActive(action) && Pony.IsActionActive(this.CurrentAction)) {
-					if(rng.Next(1,101) <= 70) direction = this.CurrentDirection;
-				}
-
-				// if pony is doing something active and she suddenly stops then this too
-				// will have a greater chance of not changing directions.
-				if(!Pony.IsActionActive(action) && Pony.IsActionActive(this.CurrentAction)) {
-					if(rng.Next(1,101) <= 70) direction = this.CurrentDirection;
-				}
-
-			} while(undecided);
-
-			int[] choice = new int[2];
-			choice[0] = action;
-			choice[1] = direction;
-			return choice;
-		}
-
-		/*
-		 * STANDING
-		 * all you have to do is look pretty.
-		 */
-
-		private void Stand(int dir) {
-			this.CurrentDirection = dir;
-			this.LoadImage();
-		}
-
-		/*
-		 * TROTTING
-		 * walking back, and forth, and back, and forth.
-		 */
-
-		private void Trot(int dir) {
-			this.CurrentDirection = dir;
-			this.LoadImage();
-
-			this.WindowTimer = new DispatcherTimer(DispatcherPriority.Normal,this.Dispatcher);
-			this.WindowTimer.Interval = TimeSpan.FromMilliseconds(25);
-			this.WindowTimer.Tick += new EventHandler(this.TrotMove);
-			this.WindowTimer.Start();
-		}
-
-		private void TrotMove(Object sender,EventArgs e) {
-
-			// if trotting to the right detect hitting the right edge.
-			if(this.CurrentDirection == 1) {
-				if((this.Left + this.Width) + 3 >= this.ScreenWidth) {
-					this.CurrentActionStop();
-					Trace.WriteLine("hit the right wall");
-					this.Trot(2);
-				} else {
-					this.Left += 3;
-				}
-			}
-
-			// if trotting to the left detect hitting the left edge.
-			if(this.CurrentDirection == 2) {
-				if(this.Left - 3 <= 0) {
-					this.CurrentActionStop();
-					Trace.WriteLine("hit the left wall");
-					this.Trot(1);
-				} else {
-					this.Left -= 3;
-				}
-			}
+			this.Pony = new Pony("Rarity");
+			this.Pony.SetWindow(this);
+			this.Pony.TellWhatDo(Pony.TROT,Pony.RIGHT);
 
 		}
 
@@ -241,31 +47,6 @@ namespace PonyApp {
 		 * removed from the form. */
 		private void OnWindowDragAction(Object sender,MouseButtonEventArgs e) {
 			DragMove();
-		}
-
-		private void LoadImage() {
-			int act = this.CurrentAction;
-			int dir = this.CurrentDirection;
-			String src = null;
-
-			switch(act) {
-				case 1: {
-						if(dir == 1) src = "pack://application:,,,/Resources/Rarity/TrotRight.gif";
-						if(dir == 2) src = "pack://application:,,,/Resources/Rarity/TrotLeft.gif";
-						break;
-					}
-				case 2: {
-						if(dir == 1) src = "pack://application:,,,/Resources/Rarity/StandRight.gif";
-						if(dir == 2) src = "pack://application:,,,/Resources/Rarity/StandLeft.gif";
-						break;
-					}
-			}
-
-			BitmapImage pony = this.CurrentImage = new BitmapImage(new Uri(src));
-			this.Width = pony.Width;
-			this.Height = pony.Height;
-			ImageBehavior.SetAnimatedSource(this.PonyImage,pony);
-
 		}
 
 	}
--- PonyApp/Pony.cs
+++ PonyApp/Pony.cs
@@ -1,19 +1,354 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Timers;
+using WpfAnimatedGif;
 
 namespace PonyApp {
 
 	public class Pony {
 
-		// actions.
+		///////////////////////////////////////////////////////////////////////
+		// constants //////////////////////////////////////////////////////////
+
+		/* action constants.
+		 * these define the different actions a pony can take during its time
+		 * on your desktop. */
+
 		public const int TROT = 1;
 		public const int STAND = 2;
 
-		// directions.
+		/* direction constants.
+		 * durr left and durr right. */
+
 		public const int RIGHT = 1;
 		public const int LEFT = 2;
+
+		///////////////////////////////////////////////////////////////////////
+		// instance properties ////////////////////////////////////////////////
+
+		// pony properties
+		private String Name;
+		private int Action;
+		private int Direction;
+
+		// physical properties
+		private MainWindow Window;
+		private BitmapImage Image;
+		private Random RNG;
+
+		// this timer is for moving the window around. unfortunately if the pc
+		// is too busy or i set it too fast, it slow down the gif rendering
+		// quite a bit as it shares the ui thread. problem is that
+		// system.timers.timer didn't have permissions to update the object
+		// owned by another thread.
+		private DispatcherTimer WindowTimer;
+
+		// this timer is for when the pony decides it should do something else.
+		// time till bordem basically.
+		private DispatcherTimer ChoiceTimer;
+
+		/* constructor Pony(string name);
+		 * constructs. give it a pony name. */
+
+		public Pony(String name) {
+			this.Name = name;
+			this.Action = 0;
+			this.Direction = 0;
+
+			this.ChoiceTimer = null;
+			this.WindowTimer = null;
+
+			this.RNG = new Random();
+		}
+
+		/* void SetWindow(Window window);
+		 * Assign a window for this pony to manipulate. do this immediately
+		 * after creating a new instance or your face will explode. */
+
+		public void SetWindow(MainWindow win) {
+			this.Window = win;
+		}
+
+		///////////////////////////////////////////////////////////////////////
+		// decision methods ///////////////////////////////////////////////////
+
+		/* void ResetChoiceTimer(void);
+		 * reset the choice timer with a value that indicates how the lively
+		 * the pony feels and how often she feels like deciding to do
+		 * something */
+
+		public void ResetChoiceTimer() {
+			// ponies in motion tend to stay in motion, while ponies at
+			// rest tend to stay at rest.
+			if(this.IsActionActive()) {
+				Trace.WriteLine("// pony feels energetic.");
+				this.ChoiceTimer.Interval = TimeSpan.FromSeconds(this.RNG.Next(3, 6));
+			} else {
+				Trace.WriteLine("// pony feels lazy.");
+				this.ChoiceTimer.Interval = TimeSpan.FromSeconds(this.RNG.Next(12, 20));
+			}
+
+			this.ChoiceTimer.Start();
+		}
+
+		/* void ChooseWhatDo(Object sender, EventArgs e);
+		 * Version of ChooseWhatDo for the ChoiceTimer callback. */
+
+		public void ChooseWhatDo(Object sender, EventArgs e) {
+			this.ChoiceTimer.Stop();
+			this.ChooseWhatDo();
+			this.ResetChoiceTimer();
+		}
+
+		/* void ChooseWhatDo(void);
+		 * do stuff that allows anypony to choose for herself what to do next */
+
+		public void ChooseWhatDo() {
+			int[] choice = this.ChooseByPersonality();
+			this.TellWhatDo(choice[0],choice[1]);
+		}
+
+		/* void TellWhatDo(int action, int direction);
+		 * tell this pony what to (even if that means she is telling herself what
+		 * to do), and do it. also if this is the first action a pony has made
+		 * then it will start the choice timer up. */
+
+		public void TellWhatDo(int action, int direction) {
+
+			Trace.WriteLine(
+				">> Action: " + this.Action.ToString() + ", "+
+				"Direction: " + this.Direction.ToString()
+			);
+
+			// no need to muck with the image and window if we are doing more of the
+			// same yeh?
+			if(action != this.Action || direction != this.Direction) {
+				this.Action = action;
+				this.Direction = direction;
+				this.StartAction();
+			}
+
+			// if this is the first action our pony has done, then we also need to
+			// spool the decision engine up.
+			if(this.ChoiceTimer == null) {
+				this.ChoiceTimer = new DispatcherTimer(DispatcherPriority.ApplicationIdle,this.Window.Dispatcher);
+				this.ChoiceTimer.Tick += new EventHandler(this.ChooseWhatDo);
+				this.ResetChoiceTimer();
+			}
+		}
+
+		/* int-array ChooseByPersonality(void);
+		 * allow the pony to choose for herself what her next actions will be.
+		 * eventually when multiple ponies are available through config files
+		 * this will handle the different personalites */
+
+		private int[] ChooseByPersonality() {
+			bool undecided = false;
+			int action = 0;
+			int direction = 0;
+
+			do {
+
+				action = this.RNG.Next(1, 3);
+				direction = this.RNG.Next(1, 3);
+				undecided = false;
+
+				// pony generally like be lazy. if she is doing somethng lazy there is a
+				// greater chance she will not want to do something active.
+				if(!this.IsActionActive() && Pony.IsActionActive(action)) {
+					if(this.RNG.Next(1, 101) <= 42) {
+						Trace.WriteLine(">> pony feels indecisive about what to do next.");
+						undecided = true;
+					}
+				}
+
+				if(direction != this.Direction) {
+					// if pony is doing something active and will continue to do so then there
+					// is a higher chance she will not change directions.
+					if(this.IsActionActive() && Pony.IsActionActive(action)) {
+						if(this.RNG.Next(1, 101) <= 70) {
+							Trace.WriteLine(">> pony's momentum carries her through");
+							direction = this.Direction;
+						}
+					}
+
+					// if pony is doing something active and she suddenly stops then this too
+					// will have a greater chance of not changing directions.
+					if(this.IsActionActive() && !Pony.IsActionActive(action)) {
+						if(this.RNG.Next(1, 101) <= 70) {
+							Trace.WriteLine(">> pony stops in her tracks");
+							direction = this.Direction;
+						}
+					}
+				}
+
+			} while(undecided);
+
+			int[] choice = new int[2];
+			choice[0] = action;
+			choice[1] = direction;
+			return choice;
+		}
+
+		///////////////////////////////////////////////////////////////////////
+		// action methods /////////////////////////////////////////////////////
+
+		/* void StartAction(void);
+		 * stop any current actions, load the image that matches, and begin the
+		 * an action sequence. */
+
+		private void StartAction() {
+
+			// stop any current action.
+			this.StopAction();
+
+			// load the new image.
+			this.LoadImage();
+
+			// place the window just above the task bar so it looks like they
+			// be walkin on it. this might be broken on multi-head or non
+			// bottom taskbars, nfi. the 10 is an offset that will need to be
+			// per pony probably.
+			this.Window.Top = SystemParameters.MaximizedPrimaryScreenHeight - this.Window.Height - 10;
+
+			switch(this.Action) {
+				case Pony.TROT:
+					this.Trot();
+					break;
+
+				case Pony.STAND:
+					this.Stand();
+					break;
+			}
+		}
+
+		/* void StopAction(void);
+		 * stop any timers and whatnot that may be screwing with the window or
+		 * image to prepare for whatever we want to do next. */
+
+		private void StopAction() {
+			if(this.WindowTimer != null) {
+				this.WindowTimer.Stop();
+				this.WindowTimer = null;
+				// this is me assuming there is a garbage collector in this
+				// language and that it is doing its goddamn job :)
+			}
+		}
+
+		/*
+		 * STANDING
+		 * all you have to do is look pretty.
+		 */
+
+		private void Stand() {
+
+		}
+
+		/*
+		 * TROTTING
+		 * walking back, and forth, and back, and forth.
+		 */
+
+		private void Trot() {
+			this.WindowTimer = new DispatcherTimer(DispatcherPriority.Normal, this.Window.Dispatcher);
+			this.WindowTimer.Interval = TimeSpan.FromMilliseconds(25);
+			this.WindowTimer.Tick += new EventHandler(this.TrotTick);
+			this.WindowTimer.Start();
+		}
+
+		private void TrotTick(Object sender, EventArgs e) {
+
+			// if trotting to the right detect hitting the right edge.
+			if(this.Direction == Pony.RIGHT) {
+				if(((this.Window.Left + this.Window.Width) + 3) >= SystemParameters.VirtualScreenWidth) {
+					Trace.WriteLine("hit the right wall");
+					this.Direction = Pony.LEFT;
+					this.LoadImage();
+				} else {
+					this.Window.Left += 3;
+				}
+			}
+
+			// if trotting to the left detect hitting the left edge.
+			if(this.Direction == Pony.LEFT) {
+				if(this.Window.Left - 3 <= 0) {
+					Trace.WriteLine("hit the left wall");
+					this.Direction = Pony.RIGHT;
+					this.LoadImage();
+				} else {
+					this.Window.Left -= 3;
+				}
+			}
+
+		}
+
+		///////////////////////////////////////////////////////////////////////
+		// image management methods ///////////////////////////////////////////
+
+		/* void Pony.LoadImage(void);
+		 * this version of the method will load the image that matches the
+		 * state of the current Action and Direction properties. */
+
+		private void LoadImage() {
+			this.LoadImage(this.Action,this.Direction);
+		}
+
+		/* void Pony.LoadImage(int action, int direction);
+		 * this version of the method will load the image for the action and
+		 * direction it is told to. */
+
+		private void LoadImage(int action,int direction) {
+			string uri = null;
+
+			switch(action) {
+				case Pony.TROT:
+					if(direction == Pony.RIGHT) uri = "pack://application:,,,/Resources/" + this.Name + "/TrotRight.gif";
+					if(direction == Pony.LEFT) uri = "pack://application:,,,/Resources/" + this.Name + "/TrotLeft.gif";
+					break;
+
+				case Pony.STAND:
+					if(direction == Pony.RIGHT) uri = "pack://application:,,,/Resources/" + this.Name + "/StandRight.gif";
+					if(direction == Pony.LEFT) uri = "pack://application:,,,/Resources/" + this.Name + "/StandLeft.gif";
+					break;
+
+			}
+
+			// here i assume garbage collection is doing its job again.
+			this.Image = new BitmapImage(new Uri(uri));
+
+			// update the window.
+			this.Window.Width = this.Image.Width;
+			this.Window.Height = this.Image.Height;
+			ImageBehavior.SetAnimatedSource(this.Window.PonyImage, this.Image);
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		// utility methods //////////////////////////////////////////////////////////
+
+		/* public Pony.IsActionActive(void);
+		 * returns if the current action the pony is doing is active or not (by means
+		 * of the static version below this) */
+
+		public bool IsActionActive() {
+			return Pony.IsActionActive(this.Action);
+		}
+
+		/* static Pony.IsActionActive(int action);
+		 * given an action type, it will return a boolean true if this action
+		 * is considered active, or false if it is considered lazy. */
 
 		public static bool IsActionActive(int action) {
 			switch(action) {
@@ -23,6 +358,7 @@ namespace PonyApp {
 					return false;
 			}
 		}
+
 	}
 
 }
