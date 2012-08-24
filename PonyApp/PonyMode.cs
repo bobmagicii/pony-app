9c995de0784d14e4eadbdada2334477c11f5a96f
Chris Benard <chris@chrisbenard.net>
2012-08-24 16:10:18 -0500

    initial refactor of bob's code

---
+++ PonyApp/PonyMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PonyApp
+{
+    [Flags]
+    public enum PonyMode
+    {
+        None = 0,
+        Free = 1,
+        Still = 2,
+        Clingy = 4
+    }
+}
